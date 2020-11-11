54ddd36f700c23dfced5119396f9217d3d429d5d
Gonzalo Berhouet <gonzaloberhouet@Gonzalos-Air.fritz.box>
2020-11-11 16:33:04 +0000

    step 05-insert html into index.html

--- src/note-controller.js
+++ src/note-controller.js
@@ -1,14 +1,3 @@
-// class NoteController {
-//   constructor(noteList) {
-//     this.noteList = noteList;
-//   }
-
-//     greeting() {
-//       let app = document.getElementById("app");
-//       console.log(app)
-//     // let newGreeting = app.replace("hello", "Howdy")
-//   }
-// }
-
-let element = document.getElementById('app')
-console.log(element)
+(function greeting() {
+  let list = (document.getElementById("app").innerHTML = "Howdy");
+})();
