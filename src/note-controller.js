bb06538ed031ef22e2739dffec3e4e976994cfb0
Beca-g <rebekahgalliano@gmail.com>
2020-11-11 17:01:35 +0000

    Created the NoteController class with methods fir ubserting HTML into div element

--- src/note-controller.js
+++ src/note-controller.js
@@ -1,3 +1,19 @@
-(function greeting() {
-  let list = (document.getElementById("app").innerHTML = "Howdy");
-})();
+class NoteController {
+  constructor(noteList = new Noteslist) {
+    this.noteList = notesList;
+    this.noteList.createStore("Favourite drink: seltzer");
+    this.noteListView = new NoteListView(this.noteslist);
+  }
+
+  insertHtml() {
+    let element = document.getElementById("app");
+    console.log(element);
+    let str = this.noteListView.getListView();
+    element.innerHTML = str;
+  }
+}
+
+let newController = new NoteController
+newController.insertHtml();
+
+
