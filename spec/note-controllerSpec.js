77e5d0875217d2ab5d9dd4030252aa6f879abb18
Gonzalo Berhouet <gonzaloberhouet@Gonzalos-Air.fritz.box>
2020-11-12 10:56:53 +0000

    step 06 finish

--- spec/note-controllerSpec.js
+++ spec/note-controllerSpec.js
@@ -1,9 +1,5 @@
 (function insertHtml() {
-  // let noteslist = new Noteslist();
-  // noteslist.createStore("Favourite drink: seltzer");
   let element = document.getElementById("app");
-  let insterNote = new NoteController();
-  insterNote.insertHtml();
   assert.isTrue(
     element.innerHTML ===
       "<ul><li><div>Favourite drink: seltzer</div></li></ul>"
